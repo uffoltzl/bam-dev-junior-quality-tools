65051f3530b9454633064b319e8768896a582485
uffoltzl <l.ufz@hotmail.fr>
2023-05-22 16:06:45 +0200

    install rntl

--- App.test.tsx
+++ App.test.tsx
@@ -1,3 +1,13 @@
-test("check jest setup", () => {
-  expect(true).toBeTruthy();
+import React from "react";
+import { View, Text } from "react-native";
+import { render, screen } from "@testing-library/react-native";
+
+it("finds rendered text", () => {
+  render(
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+
+  expect(screen.getByText("hello")).toBeTruthy();
 });
